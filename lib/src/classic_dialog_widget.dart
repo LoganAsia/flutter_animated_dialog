ec27bcd852523438b51fb1f6edcaeb09e9e6ed12
LoganAsia <166489233+LoganAsia@users.noreply.github.com>
2024-06-24 15:15:45 +0300

    Update classic_dialog_widget.dart

--- lib/src/classic_dialog_widget.dart
+++ lib/src/classic_dialog_widget.dart
@@ -1,48 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
 
-///Single selection callback of list dialog
 typedef OnSingleSelectionCallback = void Function(int selectedIndex);
 
-///Multiple selection callback of list dialog
 typedef OnMultiSelectionCallback = void Function(List<int> selectedIndexes);
 
-///
-///created time: 2019-07-31 09:35
-///author linzhiliang
-///version 1.0
-///since
-///file name: classic_dialog_widget.dart
-///description: General dialog
-///
 @immutable
 class ClassicGeneralDialogWidget extends StatelessWidget {
-  ///Title text of the dialog
+
   final String titleText;
-
-  ///Content text of the dialog
   final String contentText;
-
-  ///Text of negative button, the left button at the bottom of dialog
   final String negativeText;
-
-  ///Text of positive button, the right button at the bottom of dialog
   final String positiveText;
-
-  ///TextStyle of negative button, the left button at the bottom of dialog
   final TextStyle negativeTextStyle;
-
-  ///TextStyle of positive button, the right button at the bottom of dialog
   final TextStyle positiveTextStyle;
-
-  ///Click callback of negative button
   final VoidCallback onNegativeClick;
-
-  ///Click callback of positive button
   final VoidCallback onPositiveClick;
-
-  ///Actions at the bottom of dialog, when this is set, [negativeText] [positiveText] [onNegativeClick] [onPositiveClick] will not work。
-  final List<Widget> actions;
 
   ClassicGeneralDialogWidget({
     required this.titleText,
@@ -68,19 +41,11 @@ class ClassicGeneralDialogWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    // return CustomDialogWidget(
-    //   title: Text(
-    //     titleText,
-    //     style: Theme.of(context).dialogTheme.titleTextStyle,
-    //   ),
-    return CustomDialogWidget(
-      title: titleText != null
-          ? Text(
-              titleText,
-              style: Theme.of(context).dialogTheme.titleTextStyle,
-            )
-          : "",
+   return CustomDialogWidget(
+      title: Text(
+        titleText,
+        style: Theme.of(context).dialogTheme.titleTextStyle,
+      ),
       content: contentText != null
           ? Text(
               contentText,
@@ -126,15 +91,9 @@ class ClassicGeneralDialogWidget extends StatelessWidget {
   }
 }
 
-///List type
 enum ListType {
-  ///Single
   single,
-
-  ///Single select
   singleSelect,
-
-  ///Multiple select
   multiSelect,
 }
 
@@ -281,18 +240,18 @@ class ClassicListDialogWidgetState<T> extends State<ClassicListDialogWidget> {
       );
     } else {}
     
-      // return CustomDialogWidget(
-      // title: Text(
-      //   titleText,
-      //   style: Theme.of(context).dialogTheme.titleTextStyle,
-      // ),
-    return CustomDialogWidget(
-      title: widget.titleText != null
-          ? Text(
-              widget.titleText,
-              style: Theme.of(context).dialogTheme.titleTextStyle,
-            )
-          : "",
+      return CustomDialogWidget(
+      title: Text(
+        titleText,
+        style: Theme.of(context).dialogTheme.titleTextStyle,
+      ),
+    // return CustomDialogWidget(
+    //   title: widget.titleText != null
+    //       ? Text(
+    //           widget.titleText,
+    //           style: Theme.of(context).dialogTheme.titleTextStyle,
+    //         )
+    //       : "",
       contentPadding: EdgeInsets.all(0.0),
       content: contentWidget,
       actions: widget.actions ??
